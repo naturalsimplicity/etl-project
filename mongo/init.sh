4d73413c6695f6df8022d42e2d67c90f662f22ba
mgvasheka <matthewvasheka@gmail.com>
2025-03-12 22:29:53 +0300

    Fix files

--- mongo/init.sh
+++ mongo/init.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+USER=mongo_user
+PASSWORD=mongo_password
+HOST=localhost
+PORT=27017
+DATABASE=origin
+
+for FILE in ./data/*; do
+	mongoimport \
+  -c EventLogs \
+  --type json \
+  --file $FILE \
+  --jsonArray \
+  --authenticationDatabase=admin \
+  "mongodb://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}?authSource=admin"
+  # "mongodb://mongo_user:mongo_password@localhost:27017/origin?authSource=admin"
+done;
