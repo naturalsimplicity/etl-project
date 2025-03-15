705f6700a552f8cd9a79a9dadd410fed69c7c5f7
mgvasheka <matthewvasheka@gmail.com>
2025-03-15 22:16:32 +0300

    +

--- eshop/database/alembic/env.py
+++ eshop/database/alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from eshop.database import schema
+from database import schema
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
