56e487871626177118e8d979a17d0a465bbd2dd3
mgvasheka <matthewvasheka@gmail.com>
2025-03-15 22:55:02 +0300

    Init migration

--- eshop/database/alembic/env.py
+++ eshop/database/alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from database import schema
+import schema
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
